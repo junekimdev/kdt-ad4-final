e93beaa29cbc593353f19de442dc6635ae9a676a
June Kim <godlyjune@gmail.com>
2023-03-23 16:21:19 +0900

    Add annotations

--- app/utils/dataset.py
+++ app/utils/dataset.py
@@ -1,16 +1,17 @@
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
+import albumentations as A
 
 
 class DatasetPil(Dataset):
-    def __init__(self, file_paths):
+    def __init__(self, file_paths: list[str]) -> None:
         self.file_paths = file_paths
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.file_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Image.Image:
         file_path = self.file_paths[idx]
 
         with Image.open(file_path) as image:
@@ -18,14 +19,14 @@ class DatasetPil(Dataset):
 
 
 class DatasetPilLabel(Dataset):
-    def __init__(self, file_paths, labels):
+    def __init__(self, file_paths: list[str], labels: list[str]) -> None:
         self.file_paths = file_paths
         self.labels = labels
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.file_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> tuple[Image.Image, str]:
         label = self.labels[idx]
         file_path = self.file_paths[idx]
 
@@ -34,14 +35,14 @@ class DatasetPilLabel(Dataset):
 
 
 class DatasetAugPil(Dataset):
-    def __init__(self, file_paths, transform):
+    def __init__(self, file_paths: list[str], transform: A.Compose) -> None:
         self.file_paths = file_paths
         self.transform = transform
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.file_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Image.Image:
         file_path = self.file_paths[idx]
         image_np = np.empty(0)
 
@@ -57,15 +58,15 @@ class DatasetAugPil(Dataset):
 
 
 class DatasetAugPilLabel(Dataset):
-    def __init__(self, file_paths, labels, transform):
+    def __init__(self, file_paths: list[str], labels: list[str], transform: A.Compose) -> None:
         self.file_paths = file_paths
         self.labels = labels
         self.transform = transform
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.file_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple[Image.Image, str]:
         label = self.labels[idx]
         file_path = self.file_paths[idx]
         image_np = np.empty(0)
