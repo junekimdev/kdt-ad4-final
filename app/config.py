fe9ac970cc362e7a1ee509b6117cc84e4c22b8a5
June Kim <godlyjune@gmail.com>
2023-03-24 18:08:14 +0900

    Move class<Mode> here

--- app/config.py
+++ app/config.py
@@ -1,8 +1,25 @@
+from enum import Enum
+
+
 class Size:
     def __init__(self, c: int, h: int, w: int) -> None:
         self.c = c
         self.h = h
         self.w = w
+
+
+class Mode(Enum):
+    TRAIN = 0
+    EVAL = 1
+    TEST = 2
+
+    def str(self):
+        if self == Mode.TRAIN:
+            return "train"
+        elif self == Mode.EVAL:
+            return "eval"
+        else:
+            return "test"
 
 
 class Config:
